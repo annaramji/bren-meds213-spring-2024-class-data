5d864c721fe8ad2aaae70553c6c46a359f8fb1b1
annaramji <anna@ramji.org>
2024-04-17 10:46:36 -0700

    practicing joins, group by, algebraic operations in SQL

--- week3/script.sql
+++ week3/script.sql
@@ -99,7 +99,6 @@ SELECT AVG(Area) FROM Site; -- collapse all rows to just one output: average are
 SELECT MIN(Area) FROM Site; -- note, NULL values are ignored
 SELECT MAX(Area) FROM Site;
 
-
 -- grouping
 SELECT * FROM Site; -- view Site table, note a number of sites occur in the same Location
 -- what is the largest area for each of those locations?
@@ -137,11 +136,125 @@ SELECT Location, MAX(Area)
 -- follows the order of the clauses in the statement ^^
 -- first selecting which rows we want to operate on, then doing the grouping
 
--- seelct, grouping, further restriction on which groups you want to return
+-- select, grouping, further restriction on which groups you want to return
 SELECT Location, MAX(Area) AS Max_area
     FROM Site
     WHERE Location LIKE '%Canada'
     GROUP BY Location
     HAVING Max_area > 200; --only want to show those where the largest area is greater than 200
 
-    
+-- relational algebra peeks through!
+SELECT COUNT(*) FROM Site; -- when you do any query, returns a table 
+-- (cute baby table in this case: 1 column, column has a name, has 1 row, data type)
+-- every operation in SQL returns a table
+-- to prove it: (note: Greg put spaces at beginning and end like ( this ))
+SELECT COUNT(*) FROM ( SELECT COUNT(*) FROM Site ); -- :)
+
+-- Bird nest table
+SELECT * FROM Bird_nests LIMIT 3;
+-- ARE THERE ANY SPECIES FOR WHICH WE HAVE NO BIRD NEST DATA?
+SELECT COUNT(*) FROM Species; -- 99 TOTAL SPECIES
+-- tell me the species that we have no nest data for
+SELECT * FROM Species
+    WHERE Code NOT IN ( SELECT DISTINCT Species FROM Bird_nests ); 
+    -- 80 rows: we have data for 19 species, no data for 80 (total was 99)
+
+SELECT COUNT(Scientific_name) FROM Species; -- don't have 2 scientific names
+
+
+-- don't want momentary queries (they're gonna evaporate!)
+CREATE TEMP TABLE t AS 
+    SELECT * FROM Species
+        WHERE Code NOT IN ( SELECT DISTINCT Species FROM Bird_nests ); 
+
+SELECT * FROM t; -- and there's a table!
+
+-- TEMP keyword means that as soon as you exit out of duckdb, that table is gone. 
+-- remove TEMP if you want it to be permanent
+CREATE TABLE t_perm AS 
+    SELECT * FROM Species
+        WHERE Code NOT IN ( SELECT DISTINCT Species FROM Bird_nests ); -- without doing a join
+
+SELECT * FROM t_perm; -- saved in duckdb
+-- remove table
+DROP TABLE t_perm;
+
+-- NULL processing ----------
+-- floatAge (real number, determine egg age by floating them in water)
+SELECT COUNT(*) FROM Bird_nests
+    WHERE floatAge > 5; -- 70
+SELECT COUNT(*) FROM Bird_nests
+    WHERE floatAge <= 5; -- 97
+SELECT COUNT(*) FROM Bird_nests; -- 1547 rows... 
+-- other rows are null: if floatAge is null, null > 5, is not T or F! it's NULL
+-- tri-value logic! it's ordinal. NULL > 5? NULL.
+SELECT COUNT(*) FROM Bird_nests WHERE floatAge = NULL; -- RETURNS 0
+
+-- is NULL = NULL? "I don't know. NULL"
+SELECT COUNT(*) FROM Bird_nests WHERE floatAge IS NULL; -- RETURNS 1380
+SELECT COUNT(*) FROM Bird_nests WHERE floatAge IS NOT NULL; -- RETURNS 167
+
+-- JOINS!
+SELECT * FROM Camp_assignment; -- 441 rows
+-- full names are in Personnel table
+SELECT * FROM Personnel; -- 269 rows
+SELECT * FROM Camp_assignment JOIN Personnel
+    ON Observer = Abbreviation; -- tells how they're related
+    -- note: both columns are retained...
+
+-- could join bird nest table with species table, do an outer join to find out which species we have data for
+
+.mode csv -- to see all of the columns
+SELECT * FROM Camp_assignment JOIN Personnel
+    ON Observer = Abbreviation
+    LIMIT 3; -- tells how they're related
+
+.mode duckbox
+SELECT Year, Site, Abbreviation, Name, Start, "End" -- End is a special thing in SQL so have to put it in quotes
+    FROM Camp_assignment JOIN Personnel
+    ON Observer = Abbreviation
+    LIMIT 3;
+
+-- getting a lot of duplicated information
+SELECT * FROM Camp_assignment JOIN Personnel -- End is a special thing in SQL so have to put it in quotes
+    ON Observer = Abbreviation;
+
+
+-- more stuff about joins -- column names are ambiguous in query
+SELECT * FROM Camp_assignment JOIN Personnel
+    ON Camp_assignment.Observer = Personnel.Abbreviation; -- specifies which table they're from
+
+SELECT * FROM Camp_assignment AS ca JOIN Personnel p -- don't need to say AS for either/both
+    ON ca.Observer = p.Abbreviation;
+
+SELECT * FROM Camp_assignment ca JOIN Personnel p -- don't need to say AS
+    ON ca.Observer = p.Abbreviation;
+
+-- multiway joins
+.mode csv
+SELECT * FROM Camp_assignment ca JOIN Personnel p
+    ON ca. Observer = p.Abbreviation
+    JOIN Site s 
+    ON ca.Site = s.Code
+    LIMIT 3;
+
+.mode duckbox
+SELECT * FROM Camp_assignment ca JOIN Personnel p
+    ON ca. Observer = p.Abbreviation
+    JOIN Site s 
+    ON ca.Site = s.Code
+    WHERE ca.Observer = 'lmckinnon'
+    LIMIT 3;
+
+-- order by: at end
+SELECT * FROM Camp_assignment ca JOIN (
+    SELECT * FROM Personnel ORDER BY Abbreviation -- that ORDER BY is lost unless it's the very last step
+) p
+    ON ca. Observer = p.Abbreviation
+    JOIN Site s 
+    ON ca.Site = s.Code
+    WHERE ca.Observer = 'lmckinnon'
+    LIMIT 3;
+
+-- how many bird eggs are in each nest?
+SELECT Nest_ID, COUNT(*) FROM Bird_eggs GROUP BY Nest_ID;
