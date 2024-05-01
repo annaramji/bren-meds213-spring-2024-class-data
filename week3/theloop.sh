6c8b21e54c770e84e0f24ee7723214d868b04bf9
annaramji <anna@ramji.org>
2024-05-01 10:37:48 -0700

    wrote first bash script for this folder

---
+++ week3/theloop.sh
@@ -0,0 +1,11 @@
+# use "#!" to say it's bash (special type of command)
+# < file: read stdin from file
+# standard in (stdin) <
+# standard out (stdout) >
+# standard error (stderr) 2>
+
+#!/bin/bash
+
+for file in *.csv; do
+    echo "$file has $(wc -l < $file) lines"
+done
